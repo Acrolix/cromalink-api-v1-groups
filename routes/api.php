b55e64ed2bc2ae175a81bb5b5486c1319ded86dd
Martin Acosta <m@sis.uy>
2024-11-02 14:46:56 -0300

    add auth from api

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('oauth')->get('/hello', function (Request $request) {
+    return response()->json([
+        'message' => 'Hello World! -> API Groups v1',
+        'user' => $request->user()
+    ]);
 });
